9d491b8436775ac3ad73b29125d65e7c14509066
Yu Fong Lozano Shen <yu.lozano@gofore.com>
2023-11-12 13:09:05 +0100

    +generar baraja +generar carta +repartir carta

--- assets/js/juego.js
+++ assets/js/juego.js
@@ -6,19 +6,63 @@
  */
 
 // javascriptYDOM, ej3
+// const palos  = ["C", "D", "H", "S"];
+const valorFiguras = 10;
+const valorAce     = 11;
 
-    document.addEventListener("DOMContentLoaded", function()){
-        const palos = ["C", "D", "H", "S"];
-        const valor = [2, 3, 4, 5, 6, 7, 8, 9, 10];
-        const JQA = [10, 11];
-        const cartas = [palos, valor, JQA];
-        const jugadorCartas = document.getElementById("jugador-cartas");
-    // array para J, Q, A
-        for (const imagePath of cartas){
-            const img = document.createElement("img");
-            img.src = imagePath;
-            jugadorCartas.append(img);
+const rutaCartas   = 'assets/cartas/';
+const extension    = 'png';
+
+let cartasJugador     = [];
+let cartasComputadora = [];
+
+const baraja = generarBaraja();
+
+document.addEventListener("DOMContentLoaded", function() {
+
+    const jugadorCartas = document.getElementById('jugador-cartas');
+    const computadoraCartas = document.getElementById('computadora-cartas');
+    const btnNuevo = document.getElementById('btnNuevo');
+    const btnPedir = document.getElementById('btnPedir');
+    const btnDetener = document.getElementById('btnDetener');
+
+    btnPedir.addEventListener('click', function () {
+        repartirCarta(baraja.shift(), cartasJugador, jugadorCartas);
+        // repartirCarta(baraja.shift(), cartasComputadora, computadoraCartas);
+    });
+})
+
+function generarBaraja(a = true){
+    const palos       = "CDHS".split('');
+    const figuras      = "JQK".split('');
+    const ace         = "A";
+    const cartaMinima = 2;
+    const cartaMaxima = 10;
+
+    let baraja = [];
+
+    for(const palo of palos){
+        for(let carta = cartaMinima; carta <= cartaMaxima; carta++){
+            baraja.push(carta+palo);
         }
+        for(const figura of figuras){
+            baraja.push(figura+palo);
+        }
+        baraja.push(ace+palo);
+    }
+
+    return _.shuffle(baraja);
 }
 
+function generarElementoCarta(carta){
+    const img = document.createElement('img');
+    img.src = rutaCartas + carta + '.' + extension;
+    img.classList.add('carta');
+    return img;
+}
 
+function repartirCarta(carta, quien, contenedorCartas){
+    quien.push(carta);
+
+    contenedorCartas.append(generarElementoCarta(carta));
+}
