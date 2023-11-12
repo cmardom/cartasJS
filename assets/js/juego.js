3ea5c257bbeb8e842a62f633dc43273dccbcddd1
Yu Fong Lozano Shen <yu.lozano@gofore.com>
2023-11-12 16:31:20 +0100

    + WIP todo funcional menos bug con alert preveniendo mostrar cartas de la computadora

--- assets/js/juego.js
+++ assets/js/juego.js
@@ -7,32 +7,108 @@
 
 // javascriptYDOM, ej3
 // const palos  = ["C", "D", "H", "S"];
-const valorFiguras = 10;
-const valorAce     = 11;
-
-const rutaCartas   = 'assets/cartas/';
-const extension    = 'png';
-
-let cartasJugador     = [];
-let cartasComputadora = [];
-
-const baraja = generarBaraja();
+let esTurnoDeJugador = true;
+const puntosMaximos  = 21;
+const baraja         = generarBaraja();
 
 document.addEventListener("DOMContentLoaded", function() {
 
-    const jugadorCartas = document.getElementById('jugador-cartas');
+    const jugadorCartas     = document.getElementById('jugador-cartas');
     const computadoraCartas = document.getElementById('computadora-cartas');
-    const btnNuevo = document.getElementById('btnNuevo');
-    const btnPedir = document.getElementById('btnPedir');
-    const btnDetener = document.getElementById('btnDetener');
+    const btnNuevo          = document.getElementById('btnNuevo');
+    const btnPedir          = document.getElementById('btnPedir');
+    const btnDetener        = document.getElementById('btnDetener');
 
     btnPedir.addEventListener('click', function () {
-        repartirCarta(baraja.shift(), cartasJugador, jugadorCartas);
-        // repartirCarta(baraja.shift(), cartasComputadora, computadoraCartas);
+        const puntosJugador = repartirCarta(jugadorCartas);
+        if(puntosJugador > puntosMaximos){
+            detener();
+        }
     });
+
+    btnDetener.addEventListener('click', function(){
+       detener();
+    });
+
+    btnNuevo.addEventListener('click', function(){
+        finDePartida();
+    });
+
+    function repartirCarta(contenedorCartas){
+        const carta = baraja.shift();
+        contenedorCartas.append(generarElementoCarta(carta));
+        const puntosActuales   = calcularPuntosDeJugador(contenedorCartas);
+        const puntosNuevaCarta = calcularPuntosDeCarta(carta);
+        const puntosTotales    = puntosActuales + puntosNuevaCarta;
+
+        marcadorElement(contenedorCartas).innerHTML = puntosTotales;
+
+        return puntosTotales;
+    }
+
+    function juegaComputadora(puntosDeJugador){
+        const puntosDeComputadora = repartirCarta(computadoraCartas);
+        console.log(puntosDeComputadora);
+        if(
+            puntosDeJugador <= puntosMaximos &&
+            puntosDeComputadora < puntosDeJugador &&
+            puntosDeComputadora < puntosMaximos
+        ){
+            juegaComputadora(puntosDeJugador)
+        } else {
+            if(
+                puntosDeJugador > puntosMaximos ||
+                (puntosDeJugador < puntosDeComputadora && puntosDeComputadora <= puntosMaximos)
+            ){
+                finDePartida('La computadora Gana!');
+            } else
+            if(puntosDeComputadora > puntosMaximos || puntosDeJugador > puntosDeComputadora){
+                console.log(1);
+                finDePartida('El jugador gana!');
+            } else
+            if(puntosDeComputadora === puntosDeJugador){
+                console.log(3);
+                finDePartida('Empate!');
+            }
+        }
+    }
+
+    async function finDePartida(mensaje = ''){
+        if(mensaje){
+            console.log(mensaje);
+            setTimeout(() => {
+                alert(mensaje);
+            }, 1);
+        }
+        setTimeout(() => {
+            esTurnoDeJugador = true;
+            btnPedir.removeAttribute('disabled');
+            btnDetener.removeAttribute('disabled');
+
+            marcadorElement(jugadorCartas).innerHTML     = 0;
+            marcadorElement(computadoraCartas).innerHTML = 0;
+            jugadorCartas.innerHTML                      = '';
+            computadoraCartas.innerHTML                  = '';
+        }, 3000);
+
+    }
+
+    function detener(){
+        esTurnoDeJugador = !esTurnoDeJugador; //Toggle del booleano al valor contrario
+
+        if(!esTurnoDeJugador){
+            btnPedir.setAttribute('disabled', true);
+            btnDetener.setAttribute('disabled', true);
+
+            juegaComputadora(calcularPuntosDeJugador(jugadorCartas));
+        } else {
+            btnPedir.removeAttribute('disabled');
+            btnDetener.removeAttribute('disabled');
+        }
+    }
 })
 
-function generarBaraja(a = true){
+function generarBaraja(){
     const palos       = "CDHS".split('');
     const figuras      = "JQK".split('');
     const ace         = "A";
@@ -56,13 +132,45 @@ function generarBaraja(a = true){
 
 function generarElementoCarta(carta){
     const img = document.createElement('img');
-    img.src = rutaCartas + carta + '.' + extension;
+    img.setAttribute('src', `assets/cartas/${carta}.png`);
     img.classList.add('carta');
     return img;
 }
 
-function repartirCarta(carta, quien, contenedorCartas){
-    quien.push(carta);
+function calcularPuntosDeCarta(carta){
+    const valorFiguras = 10;
+    const valorAce     = 11;
 
-    contenedorCartas.append(generarElementoCarta(carta));
+    const numeroCarta  = carta.substring(0, carta.length - 1);
+    const valorCarta   = parseInt(numeroCarta);
+
+    if(isNaN(valorCarta)){
+        const letra = numeroCarta.toUpperCase();
+        switch(letra){
+            case 'A': return 11;
+            case 'J':
+            case 'Q':
+            case 'K':
+                return 10;
+            default:
+                throw new Error('Letra de carta no válida: ' + letra);
+        }
+    } else {
+        return valorCarta;
+    }
+}
+
+function marcadorElement(contenedorCartas){
+    return contenedorCartas
+        .previousElementSibling
+        .firstElementChild;
+}
+
+function calcularPuntosDeJugador(contenedorCartas, puntos){
+    const marcador = marcadorElement(contenedorCartas);
+    const puntosActuales = parseInt(marcador.firstChild.nodeValue);
+    if(isNaN(puntosActuales)){
+        throw new Error('Puntos actuales no es un numero');
+    }
+    return puntosActuales;
 }
